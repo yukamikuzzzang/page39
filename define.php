666b2619c32f49fa082831f77bb04a7838675d10
yukamikuzzzang <43258236+yukamikuzzzang@users.noreply.github.com>
2023-02-02 15:37:19 +0900

    test

--- define.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define( 'INSTAGRAM_APP_ID', '1321807141727479' );
-	define( 'INSTAGRAM_APP_SECRET', '150b90f6af28e0d091cba2d174d8113f' );
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://yukamikuzzzang.github.io/page39/' );
